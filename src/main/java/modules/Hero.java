9063e84561d63f80b86a1d344335ae66e2febe48
Perry Obara <perryobara@gmail.com>
2019-06-25 11:42:23 +0300

    Creates the hero object with its methods

---
+++ src/main/java/modules/Hero.java
@@ -0,0 +1,70 @@
+package modules;
+import java.util.List;
+import java.util.Objects;
+
+public class Hero {
+    private String mStrength;
+    private String mWeakness;
+    private int mAge;
+    private String mName;
+    private int id;
+
+
+    public Hero(String strength, String weakness, int age,String hero_name) {
+        this.mStrength = strength;
+        this.mWeakness = weakness;
+        this.mAge = age;
+        this.mName =hero_name;
+
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStrength(), getWeakness(), getName(), getAge());
+
+    }
+
+
+    public String getStrength() {
+        return mStrength;
+    }
+
+    public String getWeakness() {
+        return mWeakness;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public int getAge() {
+        return mAge;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setmName(String hero_name){
+        this.mName=hero_name;
+    }
+    public void setmStrength(String strength){
+        this.mName=strength;
+    }
+    public void setmAge(int age){
+        this.mAge=age;
+    }
+    public void setmWeakness(String weakness){
+        this.mWeakness=weakness;
+    }
+
+    }
+
+
+
+
